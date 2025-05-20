29b85b1b44cd704b318e939f8e185cf5b5bb09f2
bala14327 <123435756+bala14327@users.noreply.github.com>
2025-05-20 23:44:02 +0530

    Update History-routing.module.ts

--- History-routing.module.ts
+++ History-routing.module.ts
@@ -1,7 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HistoryPageComponent } from './components/history-page/history-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HistoryPageComponent
-  }
+    component: HistoryPageComponent,
+  },
 ];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HistoryRoutingModule {}
